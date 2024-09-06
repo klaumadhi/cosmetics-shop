2e3ece55c04a71d28feb6bc2818193bbc1e21503
Klaurent <klaurent.madhi@uni-weimar.com>
2024-09-06 15:47:35 +0200

    ProductDetails Page Similar Products small changes

--- src/pages/Products/ProductDetails.js
+++ src/pages/Products/ProductDetails.js
@@ -139,155 +139,158 @@ export default function ProductDetails() {
   };
 
   return (
-    <div className="container px-4 mx-auto my-10">
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
-        <div className="flex justify-center">
-          <img
-            src={selectedVariation?.variation_image || product.image}
-            alt={product.name}
-            className="rounded-lg shadow-lg object-cover max-h-[500px]"
-          />
-        </div>
-        <div className="flex flex-col justify-center">
-          <Link
-            to={`/search/${product.brand}`}
-            className="mb-2 text-lg text-gray-500"
-          >
-            {product.brand}
-          </Link>
-          <h1 className="mb-4 text-3xl font-bold text-gray-900">
-            {product.name}
-          </h1>
+    <>
+      <div className="container px-4 mx-auto my-10">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-2">
+          <div className="flex justify-center">
+            <img
+              src={selectedVariation?.variation_image || product.image}
+              alt={product.name}
+              className="rounded-lg shadow-lg object-cover max-h-[500px]"
+            />
+          </div>
+          <div className="flex flex-col justify-center">
+            <Link
+              to={`/search/${product.brand}`}
+              className="mb-2 text-lg text-gray-500"
+            >
+              {product.brand}
+            </Link>
+            <h1 className="mb-4 text-3xl font-bold text-gray-900">
+              {product.name}
+            </h1>
 
-          {/* Render Variations Section Conditionally */}
-          {product_variations?.length > 0 && (
-            <div className="mb-4">
-              {product_variations[0]?.type === "size" && (
-                <>
-                  <p className="font-semibold text-gray-700 text-md">
-                    Select Size:
-                  </p>
-                  <div className="flex gap-2 mt-2">
-                    {product_variations.map((variation) => (
-                      <button
-                        key={variation.id}
-                        onClick={() => handleVariationClick(variation)}
-                        className={`px-4 py-2 border rounded-md text-sm font-medium ${
-                          selectedVariation?.id === variation.id
-                            ? "bg-pink-600 text-white"
-                            : "bg-gray-200 text-gray-800"
-                        }`}
-                      >
-                        {variation.value}ml
-                      </button>
-                    ))}
-                  </div>
-                </>
-              )}
-
-              {product_variations[0]?.type === "color" && (
-                <div>
-                  <div className="flex gap-4 my-5">
-                    <p className="my-auto font-semibold text-gray-700 text-md">
-                      Ngjyra:
+            {/* Render Variations Section Conditionally */}
+            {product_variations?.length > 0 && (
+              <div className="mb-4">
+                {product_variations[0]?.type === "size" && (
+                  <>
+                    <p className="font-semibold text-gray-700 text-md">
+                      Select Size:
                     </p>
-                    <div className="flex inline gap-2 my-auto">
-                      {product_variations?.map((variation) => (
+                    <div className="flex gap-2 mt-2">
+                      {product_variations.map((variation) => (
                         <button
                           key={variation.id}
                           onClick={() => handleVariationClick(variation)}
-                          className={`w-8 h-8 rounded-full border-1 ${
+                          className={`px-4 py-2 border rounded-md text-sm font-medium ${
                             selectedVariation?.id === variation.id
-                              ? "border-gray-500 outline-dashed"
-                              : "border-gray-300"
+                              ? "bg-pink-600 text-white"
+                              : "bg-gray-200 text-gray-800"
                           }`}
-                          style={{
-                            backgroundImage: `url(${variation.color_image})`,
-                            backgroundSize: "cover",
-                          }}
-                        />
+                        >
+                          {variation.value}ml
+                        </button>
                       ))}
                     </div>
+                  </>
+                )}
+
+                {product_variations[0]?.type === "color" && (
+                  <div>
+                    <div className="flex gap-4 my-5">
+                      <p className="my-auto font-semibold text-gray-700 text-md">
+                        Ngjyra:
+                      </p>
+                      <div className="flex inline gap-2 my-auto">
+                        {product_variations?.map((variation) => (
+                          <button
+                            key={variation.id}
+                            onClick={() => handleVariationClick(variation)}
+                            className={`w-8 h-8 rounded-full border-1 ${
+                              selectedVariation?.id === variation.id
+                                ? "border-gray-500 outline-dashed"
+                                : "border-gray-300"
+                            }`}
+                            style={{
+                              backgroundImage: `url(${variation.color_image})`,
+                              backgroundSize: "cover",
+                            }}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                    <p className="font-semibold text-gray-700 text-md">
+                      Nuanca: {"    "}
+                      <span className="font-bold uppercase">
+                        {selectedVariation?.value}
+                      </span>
+                    </p>
                   </div>
-                  <p className="font-semibold text-gray-700 text-md">
-                    Nuanca: {"    "}
-                    <span className="font-bold uppercase">
-                      {selectedVariation?.value}
-                    </span>
-                  </p>
-                </div>
+                )}
+              </div>
+            )}
+
+            {/* Price and Discount Section */}
+            <div className="flex items-center mb-6">
+              <span className="text-2xl font-semibold text-gray-800">
+                Lek {formatNumber(currentPrice)}
+              </span>
+              {product.discount_percentage > 0 && (
+                <>
+                  <span className="ml-4 text-lg font-medium text-red-600">
+                    {product.discount_percentage}% OFF
+                  </span>
+                  <span className="ml-4 text-lg text-gray-500 line-through">
+                    {formatNumber(variationPrice)} Leke
+                  </span>
+                </>
               )}
             </div>
-          )}
 
-          {/* Price and Discount Section */}
-          <div className="flex items-center mb-6">
-            <span className="text-2xl font-semibold text-gray-800">
-              Lek {formatNumber(currentPrice)}
-            </span>
-            {product.discount_percentage > 0 && (
-              <>
-                <span className="ml-4 text-lg font-medium text-red-600">
-                  {product.discount_percentage}% OFF
-                </span>
-                <span className="ml-4 text-lg text-gray-500 line-through">
-                  {formatNumber(variationPrice)} Leke
-                </span>
-              </>
+            {/* Barcode Display */}
+            <div className="flex mb-4">
+              <p className="my-auto font-semibold text-gray-700 text-md">
+                Barcode: {"  "}
+              </p>
+              <p className="text-lg text-gray-800">
+                {selectedVariation?.barcode || product.barcode}
+              </p>
+            </div>
+
+            {/* Quantity Selector */}
+            <div className="flex items-center gap-10 mb-6">
+              <div className="flex items-center">
+                <button
+                  onClick={() => handleQuantityChange(-1)}
+                  className="px-3 py-2 bg-gray-200 rounded-md"
+                >
+                  -
+                </button>
+                <span className="px-4 text-xl">{quantity}</span>
+                <button
+                  onClick={() => handleQuantityChange(1)}
+                  className="px-3 py-2 bg-gray-200 rounded-md"
+                >
+                  +
+                </button>
+              </div>
+              <Button className="" onClick={handleAddToCart}>
+                Add {quantity} to Cart
+              </Button>
+            </div>
+
+            {/* Description Section */}
+            <h2 className="my-6 text-xl font-bold text-center font-sans text-gray-900 underline underline-offset-[3px] decoration-1">
+              Description
+            </h2>
+            <p className="leading-relaxed text-gray-700 text-md">
+              {descriptionToShow}
+            </p>
+            {product.description.split(" ").length > 40 && (
+              <button
+                onClick={toggleDescription}
+                className="flex mt-2 font-semibold text-pink-600"
+              >
+                {showFullDescription ? "Show Less" : "Show More"}
+              </button>
             )}
           </div>
-
-          {/* Barcode Display */}
-          <div className="flex mb-4">
-            <p className="my-auto font-semibold text-gray-700 text-md">
-              Barcode: {"  "}
-            </p>
-            <p className="text-lg text-gray-800">
-              {selectedVariation?.barcode || product.barcode}
-            </p>
-          </div>
-
-          {/* Quantity Selector */}
-          <div className="flex items-center gap-10 mb-6">
-            <div className="flex items-center">
-              <button
-                onClick={() => handleQuantityChange(-1)}
-                className="px-3 py-2 bg-gray-200 rounded-md"
-              >
-                -
-              </button>
-              <span className="px-4 text-xl">{quantity}</span>
-              <button
-                onClick={() => handleQuantityChange(1)}
-                className="px-3 py-2 bg-gray-200 rounded-md"
-              >
-                +
-              </button>
-            </div>
-            <Button className="" onClick={handleAddToCart}>
-              Add {quantity} to Cart
-            </Button>
-          </div>
-
-          {/* Description Section */}
-          <h2 className="my-6 text-xl font-bold text-center font-sans text-gray-900 underline underline-offset-[3px] decoration-1">
-            Description
-          </h2>
-          <p className="leading-relaxed text-gray-700 text-md">
-            {descriptionToShow}
-          </p>
-          {product.description.split(" ").length > 40 && (
-            <button
-              onClick={toggleDescription}
-              className="flex mt-2 font-semibold text-pink-600"
-            >
-              {showFullDescription ? "Show Less" : "Show More"}
-            </button>
-          )}
         </div>
-      </div>
 
-      <div className="mt-10">
+        {showPopup && <Popup onClose={() => setShowPopup(false)} />}
+      </div>
+      <div className="my-10">
         <h2 className="mb-6 font-sans text-xl font-bold text-center text-gray-900">
           Similar Products
         </h2>
@@ -307,8 +310,6 @@ export default function ProductDetails() {
           )}
         </div>
       </div>
-
-      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
-    </div>
+    </>
   );
 }
